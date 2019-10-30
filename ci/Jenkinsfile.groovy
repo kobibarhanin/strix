985dd592af95c376e4b2455705c17b127f52c946
Kobi Bar-Hanin <kobibarhanin@gmail.com>
2019-10-30 17:27:57 +0200

    jenkins play

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -17,7 +17,8 @@ pipeline {
                     echo "running"
                     for (i=0; i<5; i++){
                         Integer x = i;
-                        build job: 'job_temp', parameters: [labels:'agent_1', string(name: 'ID', value: x.toString())]
+                        build job: 'job_temp', parameters: [[$class: 'NodeParameterValue', labels: 'agent_1', nodeEligibility: [$class: 'AllNodeEligibility']],
+                        string(name: 'ID', value: x.toString())]
                     }
                 }
             }
