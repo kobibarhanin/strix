c79df1997142c867995aa362ca9d0a7d98f7d07f
Kobi Bar-Hanin <kobibarhanin@gmail.com>
2019-10-30 16:59:55 +0200

    jenkins play

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -15,7 +15,7 @@ pipeline {
             steps {
                 script {
                     echo "running"
-                    for (i=0; i<3; i++){
+                    for (i=0; i<300; i++){
                         Integer x = i;
                         build job: 'job_temp', parameters: [string(name: 'ID', value: x.toString())]
                     }
