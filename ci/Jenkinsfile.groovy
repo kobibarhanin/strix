2ff75cfefd1806bc4def3c4670e00be2d012f11f
Kobi Bar-Hanin <kobibarhanin@gmail.com>
2019-10-30 16:20:21 +0200

    jenkins play

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -15,9 +15,6 @@ pipeline {
             steps {
                 script {
                     echo "running"
-                    for (i = 0; i <3; i++) {
-                       System.out.println("Hello World")
-                    }
                 }
             }
         }
