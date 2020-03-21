0ffe79ab80ee764be61892efaf2881ead86a3aed
Kobi Bar-Hanin <kobibarhanin@gmail.com>
2020-03-21 22:14:41 +0200

    fast commit

--- ci/ci.sh
+++ ci/ci.sh
@@ -6,6 +6,7 @@ echo "======================================="
 
 cd agent
 
+docker network create mynet
 docker run -d -p 27017:27017 --network=mynet --name bitz_0_db mongo
 ../setup/agentctl restart bitz_0 bitz_0 5000 tracker host rebuild bitz_0_db 27017
 
